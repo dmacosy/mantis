a74204e216ddc2faa4fd665adb96662af7386ae7
Dedric <dedric@blueacorn.com>
2013-09-09 14:33:16 -0400

    first commit

---
+++ config_inc.php
@@ -0,0 +1,10 @@
+<?php
+	$g_hostname = 'localhost';
+	$g_db_type = 'mysql';
+	$g_database_name = 'mantis';
+	$g_db_username = 'root';
+	$g_db_password = 'root';
+    $g_allow_anonymous_login = ON;
+    $g_anonymous_account = 'blueacorn';
+    $g_allow_blank_email = ON;
+?>
